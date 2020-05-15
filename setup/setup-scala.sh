8c426f30d27b356cc2ceb0ae6d7fb8664f7d5a60
suzuki-navi <suzuki.navi.dev@gmail.com>
2020-05-15 16:53:03 +0900

    improve setup/setup-scala.sh

--- setup/setup-scala.sh
+++ setup/setup-scala.sh
@@ -1,6 +1,10 @@
 
 bash $(dirname "$0")/download-scala.sh
 
+if ! type java >/dev/null 2>&1; then
+    . $HOME/.mydocker/setup/setup-openjdk-13.sh
+fi
+
 export SCALA_HOME=$HOME/.mydocker/var/host/scala
 export PATH=$SCALA_HOME/bin:$PATH
 
